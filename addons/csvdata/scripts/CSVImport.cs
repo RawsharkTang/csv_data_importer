cd3614f4139319e9d45773f1484152228fbe93a1
Rawshark <notapotato26184@gmail.com>
2025-05-29 23:27:47 +0800

    using stringName for storage

--- addons/csvdata/scripts/CSVImport.cs
+++ addons/csvdata/scripts/CSVImport.cs
@@ -2,77 +2,62 @@ using Godot;
 using Godot.Collections;
 using nietras.SeparatedValues;
 using System;
-
-public partial class CSVImport : EditorImportPlugin
-{
+[Tool]
+public partial class CSVImport : EditorImportPlugin {
     [Flags]
-    public enum Presets
-    {
+    public enum Presets {
         CSV = 1 << 0,
 
     }
     [Flags]
-    public enum Delimiters
-    {
+    public enum Delimiters {
         Auto = 1 << 0,
         COMMA = 1 << 1,
         TAB = 1 << 2
     }
-    public override string _GetImporterName()
-    {
+    public override string _GetImporterName() {
         return "luoshark.csv_resource";
     }
 
-    public override string _GetVisibleName()
-    {
+    public override string _GetVisibleName() {
         return "CSVResource";
     }
 
-    public override float _GetPriority()
-    {
+    public override float _GetPriority() {
         return 2.0f;
     }
 
-    public override int _GetImportOrder()
-    {
+    public override int _GetImportOrder() {
         return 0;
     }
 
-    public override string[] _GetRecognizedExtensions()
-    {
+    public override string[] _GetRecognizedExtensions() {
         return ["csv", "tsv"];
     }
 
 
-    public override string _GetResourceType()
-    {
+    public override string _GetResourceType() {
         return "Resource";
     }
 
-    public override string _GetSaveExtension()
-    {
+    public override string _GetSaveExtension() {
         return "tres";
     }
 
-    public override int _GetPresetCount()
-    {
+    public override int _GetPresetCount() {
         return 1;
     }
 
-    public override string _GetPresetName(int presetIndex)
-    {
+    public override string _GetPresetName(int presetIndex) {
         return Enum.GetName((Presets)(1 << presetIndex));
     }
-    public override bool _GetOptionVisibility(string path, StringName optionName, Dictionary options)
-    {
+    public override bool _GetOptionVisibility(string path, StringName optionName, Dictionary options) {
         return true;
     }
-    public override Array<Dictionary> _GetImportOptions(string path, int presetIndex)
-    {
+    public override Array<Dictionary> _GetImportOptions(string path, int presetIndex) {
         var p = (Presets)(1 << presetIndex);
         Array<Dictionary> hint_dict = [];
-        if (p == Presets.CSV)
-        {
+        if (p == Presets.CSV) {
             hint_dict.Add(new Dictionary()
             {
                 {"name","Parse"},
@@ -94,11 +79,9 @@ public partial class CSVImport : EditorImportPlugin
         return hint_dict;
     }
 
-    public override Error _Import(string sourceFile, string savePath, Dictionary options, Array<string> platformVariants, Array<string> genFiles)
-    {
+    public override Error _Import(string sourceFile, string savePath, Dictionary options, Array<string> platformVariants, Array<string> genFiles) {
         var file = FileAccess.Open(sourceFile, FileAccess.ModeFlags.Read);
-        if (FileAccess.GetOpenError() != Error.Ok)
-        {
+        if (FileAccess.GetOpenError() != Error.Ok) {
             GD.Print($"Failed to open file: {sourceFile}");
             return Error.Failed;
         }
@@ -107,8 +90,7 @@ public partial class CSVImport : EditorImportPlugin
         var skip_row = options["SkipRow"].AsInt32();
         skip_row = parse ? skip_row + 1 : skip_row;
         char delimiter_char = ',';
-        switch (delimiter)
-        {
+        switch (delimiter) {
             case Delimiters.COMMA: delimiter_char = ','; break;
             case Delimiters.TAB: delimiter_char = '\t'; break;
             default: break;
@@ -122,28 +104,22 @@ public partial class CSVImport : EditorImportPlugin
         // GD.Print(string.Join(",", headers.ColNames));
 
         var csv_resource = new CSVResource();
-        if (!headers.IsEmpty)
-        {
+        if (!headers.IsEmpty) {
             csv_resource._headers = [.. headers.ColNames];
             csv_resource._data = [];
         }
-        if (!reader.IsEmpty)
-        {
+        if (!reader.IsEmpty) {
             var type_row = parse && reader.MoveNext() ? ParseType(reader.Current, headers) : []; //Extract the types
-            foreach (var row in reader)
-            {
+            foreach (var row in reader) {
                 if (row.RowIndex <= skip_row) continue;
                 // var a = row[1];
                 var value_dict = new Dictionary();
                 var key = row[0].ToString();
-                foreach (var h in csv_resource._headers)
-                {
+                foreach (var h in csv_resource._headers) {
                     Variant v = Variant.From(row[h].ToString());
-                    if (parse)
-                    {
+                    if (parse) {
                         type_row.TryGetValue(h, out string type_name);
-                        switch (type_name)
-                        {
+                        switch (type_name) {
                             case "string":
                                 v = Variant.From(row[h].Parse<string>());
                                 // value_dict.Add(h, Variant.From(row[h].Parse<string>()));
@@ -183,18 +159,15 @@ public partial class CSVImport : EditorImportPlugin
         reader.Dispose();
         var file_name = $"{savePath}.{_GetSaveExtension()}";
         var err = ResourceSaver.Save(csv_resource, file_name);
-        if (err != Error.Ok)
-        {
+        if (err != Error.Ok) {
             GD.PrintErr($"Failed to save resource {file_name}");
         }
         return err;
     }
 
-    public System.Collections.Generic.Dictionary<string, string> ParseType(SepReader.Row current, SepReaderHeader header)
-    {
+    public System.Collections.Generic.Dictionary<string, string> ParseType(SepReader.Row current, SepReaderHeader header) {
         var dict = new System.Collections.Generic.Dictionary<string, string>();
-        foreach (var h in header.ColNames)
-        {
+        foreach (var h in header.ColNames) {
             dict.Add(h, current[h].ToString());
         }
         return dict;
--- addons/csvdata/scripts/CSVResource.cs
+++ addons/csvdata/scripts/CSVResource.cs
@@ -1,40 +1,38 @@
 using System.Collections.Generic;
 using Godot;
-
-public partial class CSVResource : Resource
-{
+[GlobalClass, Tool]
+public partial class CSVResource : Resource {
     [Export]
-    public Godot.Collections.Array<string> Headers
-    {
+    public Godot.Collections.Array<StringName> Headers {
         get => [.. _headers]; // Convert List to Godot Array
         set => _headers = [.. value]; // Convert Godot Array to List
     }
 
-    public List<string> _headers = [];
+    public List<StringName> _headers = [];
 
+    /// <summary>
+    /// Exported Data, for c# script use _data directly.
+    /// </summary>
     [Export]
-    public Godot.Collections.Dictionary<string, Godot.Collections.Dictionary> Data
-    {
+    public Godot.Collections.Dictionary<StringName, Godot.Collections.Dictionary> Data {
         get => GetData();
         set => SetData(value);
     }
 
 
-    public Dictionary<string, Godot.Collections.Dictionary> _data = [];
+    public Dictionary<StringName, Godot.Collections.Dictionary> _data = [];
 
-    public Godot.Collections.Dictionary<string, Godot.Collections.Dictionary> GetData()
-    {
-        return new Godot.Collections.Dictionary<string, Godot.Collections.Dictionary>(_data);
+    public Godot.Collections.Dictionary<StringName, Godot.Collections.Dictionary> GetData() {
+        return new Godot.Collections.Dictionary<StringName, Godot.Collections.Dictionary>(_data);
     }
 
-    public void SetData(Godot.Collections.Dictionary<string, Godot.Collections.Dictionary> value)
-    {
-        _data = new Dictionary<string, Godot.Collections.Dictionary>(value);
+    public void SetData(Godot.Collections.Dictionary<StringName, Godot.Collections.Dictionary> value) {
+        _data = new Dictionary<StringName, Godot.Collections.Dictionary>(value);
     }
 
-    public Godot.Collections.Dictionary Fetch(string key)
-    {
+    public Godot.Collections.Dictionary Fetch(string key) {
         _data.TryGetValue(key, out var value);
         return value;
     }
+
 }
